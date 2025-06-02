bc93fd69c5a9920c48802abe4b2298da1e7d74e2
simonperneel <Simon.Perneel@UGent.be>
2025-06-02 14:04:15 +0200

    Update portfolio.ts

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -10,7 +10,7 @@ export interface Portfolio {
 export const portfolioData: Portfolio[] = [
   // Example entry
   {
-    title: "Optimizing human activity recogntion with inertial sensors",
+    title: "Optimizing human activity recognition with inertial sensors",
     description:
       "For my master's thesis, I developed a human activity recognition system using wearable Magnetic Inertial Measurement Units (MIMUs). I compared feature-based and 'raw' inertial data-based classification approaches, evaluating sensor placement, feature selection, and the value of magnetometer data. The final model, using only accelerometer and gyroscope features from 3 optimally placed sensors, achieved an accuracy of 97% and F1-score of 98% on 7 common physical activities.",
     technologies: ["Python", "Sklearn", "Tensorflow"],

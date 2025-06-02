2920f8eca90c802a08f57ffbf72844c66b2124d8
simonperneel <Simon.Perneel@UGent.be>
2025-06-02 13:53:15 +0200

    chagne portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -12,7 +12,7 @@ export const portfolioData: Portfolio[] = [
   {
     title: "Optimizing human activity recogntion with inertial sensors",
     description:
-      "For my master's thesis, I developed a human activity recognition system using wearable Magnetic Inertial Measurement Units (MIMUs). I compared feature-based and 'raw' inertial data-based classification approaches, evaluating sensor placement, feature selection, and the value of magnetometer data. The final model, using only accelerometer and gyroscope features from 3 optimally placed sensors, achieved an accuracy of 97% and F1-score of 98%.",
+      "For my master's thesis, I developed a human activity recognition system using wearable Magnetic Inertial Measurement Units (MIMUs). I compared feature-based and 'raw' inertial data-based classification approaches, evaluating sensor placement, feature selection, and the value of magnetometer data. The final model, using only accelerometer and gyroscope features from 3 optimally placed sensors, achieved an accuracy of 97% and F1-score of 98% on 7 common physical activities.",
     technologies: ["Python", "Sklearn", "Tensorflow"],
     projectUrl: "https://github.com/simonperneel/MAI-Thesis-HAR/",
     codeUrl: "https://github.com/username/project",
